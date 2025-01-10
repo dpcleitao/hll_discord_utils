989f5d3e2de132f0863897184b283b1f91611476
Dan Leitao <3083151+dpcleitao@users.noreply.github.com>
2025-01-10 18:40:19 +0100

    Added back in guild intent

--- rcon/discord/bot.py
+++ rcon/discord/bot.py
@@ -20,6 +20,7 @@ class MainBot(commands.Bot):
         intents = discord.Intents.default()
         intents.message_content = True
         intents.members = True
+        intents.guilds = True
         
         super().__init__(
             command_prefix="/", 
